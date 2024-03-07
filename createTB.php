358c72f47e60496909947b0d6a90cba4c091b28e
prince19122002 <159906531+prince19122002@users.noreply.github.com>
2024-03-08 01:30:56 +0530

    Update createTB.php

--- createTB.php
+++ createTB.php
@@ -10,35 +10,32 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if(!$conn){
     die("Failed to connect" . mysqli_connect_error());
 }else{
-     echo "Connection  successful";
+     echo "Connection successful";
 }
 echo "<br>";
 // Create a table in the DB
-$sql = "CREATE TABLE `users` (
+$sql = "CREATE TABLE `users` ( 
  `ID` INT(50) NOT NULL AUTO_INCREMENT ,
- `name` VARCHAR(100) NOT NULL ,
- 
- `phoneno` VARCHAR(100) NOT NULL ,
- `fathername` VARCHAR(100) NOT NULL ,
- 
- `class` VARCHAR(100) NOT NULL ,
- `gender` VARCHAR(100) NOT NULL ,
-
- `email` VARCHAR(100) NOT NULL ,
- `note` VARCHAR(100) NOT NULL ,
-
- `DOB` VARCHAR(100) NOT NULL ,
- `AccCreatedOn` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
-
- `status` VARCHAR(100) NOT NULL ,
- `createdby` VARCHAR(100) NOT NULL ,
+ `Name` VARCHAR(100) NOT NULL ,
+ `FathersName` VARCHAR(100) NOT NULL , 
+ `Phone.no` VARCHAR(100) NOT NULL , 
+ `Email` VARCHAR(100) NOT NULL , 
+ `Class` VARCHAR(100) NOT NULL , 
+ `Gender` VARCHAR(100) NOT NULL , 
+ `Note` VARCHAR(100) NOT NULL , 
+ `DOB` VARCHAR(100) NOT NULL , 
+ `Acc.CreatedOn` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+ `Status` VARCHAR(100) NOT NULL ,
+ `Acc.CreatedBy` VARCHAR(100) NOT NULL , 
  PRIMARY KEY (`ID`))" ;
+
 $result = mysqli_query($conn, $sql);
+
 // Check for the table creation success
 if($result){
-    echo "The tables is created ";
+    echo "The tables  created successfully!<br>";
 }
 else{
-    echo "The tables is not created ". mysqli_error($conn);
+    echo "s not created successfully ". mysqli_error($conn);
 }
 ?>
